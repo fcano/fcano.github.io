bebce7da91605dc0d429a11647c6f1cac2371fbd
fcano <florencio.cano@gmail.com>
2021-05-20 20:38:32 +0200

    Change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,4 +27,4 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "pelican-themes/subtle"
+THEME = "pelican-themes/bootlex"
