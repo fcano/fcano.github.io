38ea7310bc96853082b0bf62d9eee15360fd02fc
fcano <florencio.cano@gmail.com>
2021-05-21 16:12:26 +0200

    Add analytics

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = 'cybersecurity-notes'
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "UA-197636836-1"
